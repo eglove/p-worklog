7210c0678b32d3bc2ec1660e3a26ee7bf90c4add
Ethan Glover <hello@ethang.email>
2021-03-24 18:51:01 -0500

    fix

--- index.php
+++ index.php
@@ -4,4 +4,4 @@ declare(strict_types=1);
 require('HelperJira.php');
 
 $jiraJson = json_decode(file_get_contents('https://pastebin.com/raw/ciLaHmWs'), true);
-Helper_Jira::logWorklog($jiraJson);
+HelperJira::logWorklog($jiraJson);
